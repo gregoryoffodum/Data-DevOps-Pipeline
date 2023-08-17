26279e316e04bab703b821c0d0093cf137ea34ca
Gregory Offodum <78843321+gregoryoffodum@users.noreply.github.com>
2023-08-17 15:15:31 -0400

    Delete download.sh.sh

--- download.sh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-for year in `seq 2016 2018`;do for month in `seq 1 12`;do wget --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=5051&Year=${year}&Month=${month}&Day=14&timeframe=3&submit= Download+Data" ;done;done
-
